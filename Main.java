8e671105f5405bccf476c6d80588b4699546b279
anustubhmishra <anustubhmishra@gmail.com>
2025-10-08 11:31:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+// Student class must implement Serializable
+class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    int studentID;
+    String name;
+    String grade;
+
+    public Student(int studentID, String name, String grade) {
+        this.studentID = studentID;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void display() {
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Name: " + name);
+        System.out.println("Grade: " + grade);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String filename = "student.ser";
+
+        // Serialization
+        Student s1 = new Student(101, "Aman", "A");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(s1);
+            System.out.println("Student object serialized successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialization
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            Student s2 = (Student) ois.readObject();
+            System.out.println("\nDeserialized Student Data:");
+            s2.display();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
